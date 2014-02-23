9d68913568a8b5d1c6590afa36ab79737c058267
Daniel Triendl <daniel@pew.cc>
2014-02-23 14:29:50 +0100

    Allow custom attributes in Forms

--- view/forms.php
+++ view/forms.php
@@ -6,31 +6,51 @@ class Forms {
 	public static function form($action) {
 		$csrf = new CSRF();
 		$form = '<form action="' . $action . '" method="post">';
-		$form .= '<input type="hidden" id="' . $csrf->getName() . '" name="' . $csrf->getName() . '" value="' . $csrf->getToken() . '" />';
+		$form .= static::inputHidden($csrf->getName(), $csrf->getToken());
 		return $form;
 	}
 	
-	public static function input($type, $name, $value = null) {
+	public static function input($type, $name, $value = null, $attrib = null) {
 		switch ($type) {
 			case 'text':
-				return static::inputText($name, $value);
-			case 'password':
-				return static::inputPassword($name);
+			case 'hidden':
 			case 'submit':
-				return static::inputSubmit($name, $value);
+				return '<input' . static::getAttributes(['type' => $type, 'id' => $name, 'name' => $name, 'value' => $value]) . static::getAttributes($attrib) . ' />';
+			case 'password':
+				return static::inputPassword($name, $attrib);
+			case 'textarea':
+				return static::textarea($name, $value, $attrib);
 		}
 	}
 
-	public static function inputText($name, $value) {
-		return '<input id="' . $name . '" type="text" name="' . $name . '" value="' . filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . '" />';
+	public static function inputText($name, $value, $attrib = null) {
+		return static::input('text', $name, $value, $attrib);
 	}
 	
-	public static function inputPassword($name) {
+	public static function inputPassword($name, $attrib = null) {
 		return '<input id="' . $name . '" type="password" name="' . $name . '" />';
 	}
 	
-	public static function inputSubmit($name, $value) {
-		return '<input id="' . $name . '" type="submit" name="' . $name . '" value="' . filter_Var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . '" />';
+	public static function inputSubmit($name, $value, $attrib = null) {
+		return static::input('submit', $name, $value, $attrib);
+	}
+	
+	public static function inputHidden($name, $value, $attrib = null) {
+		return static::input('hidden', $name, $value, $attrib);
+	}
+	
+	public static function textarea($name, $value, $attrib = null) {
+		return '<textarea' . static::getAttributes(['id' => $name, 'name' => $name]) . static::getAttributes($attrib) . '>' . filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . '</textarea>';
+	}
+	
+	public static function getAttributes($attrib) {
+		$result = '';
+		if ($attrib !== null) {
+			foreach ($attrib as $name => $value) {
+				$result .= ' ' . $name . '="' . filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS) . '"';
+			}
+		}
+		return $result;
 	}
 }
 
