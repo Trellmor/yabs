8b6d7309b97c9e90884bf273aa206b45a07528e2
Daniel Triendl <daniel@pew.cc>
2014-03-01 19:20:35 +0100

    Updated translation

--- view/default/widget_latestcomments.php
+++ view/default/widget_latestcomments.php
@@ -6,7 +6,7 @@ use View\HTML;
   <h2><?php echo _('Comments'); ?></h2>
   <?php foreach ($comments as $comment): ?>
   <p>
-    <?php echo _('Von'); ?> <strong><?php echo HTML::wordwrap($comment->getAuthor()); ?></strong><br />
+    <?php echo _('By'); ?> <strong><?php echo HTML::wordwrap($comment->getAuthor()); ?></strong><br />
     <?php echo HTML::wordwrap(substr(HTML::strip($comment->getText()), 0, 140)); ?><br />
     <a href="<?php HTML::out(Uri::to('blog/' . $comment->getEntryUri())); ?>"><?php HTML::out($comment->getEntryTitle()); ?></a>
   </p>
