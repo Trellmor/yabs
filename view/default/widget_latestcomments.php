9378629d4f72483838a021ad6741857e16483eb6
Daniel Triendl <daniel@pew.cc>
2014-03-08 00:52:19 +0100

    Link to comment instead of just to the post

--- view/default/widget_latestcomments.php
+++ view/default/widget_latestcomments.php
@@ -8,7 +8,7 @@ use View\HTML;
   <p>
     <?php echo _('By'); ?> <strong><?php echo HTML::wordwrap($comment->getAuthor()); ?></strong><br />
     <?php echo HTML::wordwrap(substr(HTML::strip($comment->getText()), 0, 140)); ?><br />
-    <?php echo _('In'); ?> <a href="<?php HTML::out(Uri::to('blog/' . $comment->getEntryUri())); ?>"><?php HTML::out($comment->getEntryTitle()); ?></a>
+    <?php echo _('In'); ?> <a href="<?php HTML::out(Uri::to('blog/' . $comment->getEntryUri()) . '#com' . $comment->getId()); ?>"><?php HTML::out($comment->getEntryTitle()); ?></a>
   </p>
   <?php endforeach; ?>
 </li>
